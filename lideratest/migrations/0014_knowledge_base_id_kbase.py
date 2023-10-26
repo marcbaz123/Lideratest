3cf5790f50a731ac888a783358f0f04d72d69964
marcbaz123 <57149572+marcbaz123@users.noreply.github.com>
2023-10-26 09:18:34 -0300

    update migrations knowledge

--- lideratest/migrations/0014_knowledge_base_id_kbase.py
+++ lideratest/migrations/0014_knowledge_base_id_kbase.py
@@ -35,7 +35,7 @@ class Migration(migrations.Migration):
         migrations.AddField( 
             model_name='knowledge_base',
             name='id_kbase',
-            field=models.AutoField(auto_created=True, db_column='id', default=1, primary_key=True, serialize=False),
+            field=models.AutoField(auto_created=True, db_column='id', primary_key=True, serialize=False),
             
         ),
         migrations.RunPython(cargar_datos_iniciales),
