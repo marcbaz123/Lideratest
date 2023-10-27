bec8817f15827946e027940c5a4da70d47f3baf7
marcbaz324 <marcbaz324@hotmail.com>
2023-10-27 19:56:50 -0300

    Agregar campos de preguntas con resultados de dimensiones 2

--- djangolidera/settings.py
+++ djangolidera/settings.py
@@ -80,13 +80,13 @@ WSGI_APPLICATION = 'djangolidera.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    #'default': dj_database_url.config(
-     #   default='postgresql://postgres:postgres@localhost/postgres',
-      #  conn_max_age=600
-    #)
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost/postgres',
+        conn_max_age=600
+    )
 
-   'default':{'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',}
+ #  'default':{'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': BASE_DIR / 'db.sqlite3',}
   #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
    #     'NAME': 'lideratest-db',
     #    'USER': 'postgres',
